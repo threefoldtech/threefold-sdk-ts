0971c800dec44dea5f1ee68ddbaa6638c2b36421
maayarosama <mayarosama18@gmail.com>
2024-11-19 12:12:26 +0200

    Fixing manual link redirects to connect wallet docs

--- packages/playground/src/utils/manual.ts
+++ packages/playground/src/utils/manual.ts
@@ -10,6 +10,7 @@ export const manual = {
   dao: `${BASE}/documentation/dashboard/tfchain/tf_dao.html`,
   caprover: `${BASE}/documentation/dashboard/solutions/caprover.html`,
   tf_connect_app: `${BASE}/documentation/threefold_token/storing_tft/tf_connect_app.html`,
+  tf_connect_wallet: `${BASE}/documentation/dashboard/wallet_connector.html`,
   contract_locking: `${BASE}/documentation/developers/tfchain/tfchain.html#contract-locking`,
   billing_pricing: `${BASE}/documentation/dashboard/deploy/node_finder.html#billing--pricing`,
   discount_levels: `${BASE}/knowledge_base/cloud/pricing/pricing.html#staking-discount`,
