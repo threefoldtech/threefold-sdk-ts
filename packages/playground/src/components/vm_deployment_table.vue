8a4b7586930492f232ea337fbf522a37629b5a82
Amira <56790126+amiraabouhadid@users.noreply.github.com>
2024-11-21 10:04:31 +0200

    set sortable to false in backends column (#3659)

--- packages/playground/src/components/vm_deployment_table.vue
+++ packages/playground/src/components/vm_deployment_table.vue
@@ -286,6 +286,7 @@ const filteredHeaders = computed(() => {
         value(item: any) {
           return item[0].workloads[0].data.backends.join(", ");
         },
+        sortable: false,
       },
       {
         title: "Domain",
