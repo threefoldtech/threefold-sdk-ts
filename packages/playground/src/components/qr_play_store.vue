98851503640c5376da1d089a7a83f5b2f4872cb2
amiraabouhadid <amiraabouhadid@gmail.com>
2024-11-27 08:29:17 +0200

    remove light app store btns

--- packages/playground/src/components/qr_play_store.vue
+++ packages/playground/src/components/qr_play_store.vue
@@ -12,7 +12,7 @@
         :style="{ cursor: 'pointer', width: '9rem' }"
         class="mx-2"
         :title="app.alt"
-        v-html="theme == 'dark' ? app.src_dark : app.src_light"
+        v-html="app.src"
         :href="app.url"
         target="_blank"
       />
@@ -23,18 +23,15 @@
 const props = defineProps({
   qr: String,
 });
-const theme = useTheme().name.value;
+
 const apps = [
   {
-    src_dark: `<img width="128"  src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Google_Play_Store_badge_EN.svg/128px-Google_Play_Store_badge_EN.svg.png?20220907104002">`,
-    src_light: `<img width="128"  badge" src="/images/icons/g_play.png"/> 
-`,
+    src: `<img width="128"  src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Google_Play_Store_badge_EN.svg/128px-Google_Play_Store_badge_EN.svg.png?20220907104002">`,
     alt: "Threefold Connect on Google Play Store",
     url: "https://play.google.com/store/apps/details?id=org.jimber.threebotlogin&hl=en&gl=US",
   },
   {
-    src_dark: `<img width="128"  src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Download_on_the_App_Store_RGB_blk.svg/128px-Download_on_the_App_Store_RGB_blk.svg.png?20180317110059"/>`,
-    src_light: `<img width="128"  src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b3/Download_on_the_App_Store_Badge_US-UK_RGB_wht.svg/1280px-Download_on_the_App_Store_Badge_US-UK_RGB_wht.svg.png"/>`,
+    src: `<img width="128"  src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Download_on_the_App_Store_RGB_blk.svg/128px-Download_on_the_App_Store_RGB_blk.svg.png?20180317110059"/>`,
     alt: "Threefold Connect on Apple App Store",
     url: "https://apps.apple.com/us/app/threefold-connect/id1459845885",
   },
@@ -42,7 +39,6 @@ const apps = [
 </script>
 <script lang="ts">
 import { defineComponent } from "vue";
-import { useTheme } from "vuetify/lib/framework.mjs";
 
 import QrcodeGenerator from "../components/qrcode_generator.vue";
 
