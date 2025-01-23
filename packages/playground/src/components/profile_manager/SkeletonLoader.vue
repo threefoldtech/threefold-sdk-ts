2acadf21f3782a9613200035aa2f28aa5111ed14
Omar Kassem <omarkassem099@gmail.com>
2025-01-23 21:59:50 +0200

    Feature(ProfileManager): add suspense to fallback to the skeletonLoader if one of the commponents is still loading

--- packages/playground/src/components/profile_manager/SkeletonLoader.vue
+++ packages/playground/src/components/profile_manager/SkeletonLoader.vue
@@ -1,19 +1,21 @@
 <template>
-  <v-row>
-    <v-col cols="12" md="9">
-      <v-skeleton-loader type="text" height="56" />
-    </v-col>
-    <v-col cols="12" md="3">
-      <v-skeleton-loader type="text" height="56" />
-    </v-col>
-  </v-row>
-  <v-skeleton-loader type="alert" />
-  <v-skeleton-loader id="test" type="text" height="56" />
-  <v-skeleton-loader type="text" height="56" />
-  <v-skeleton-loader type="text" height="56" />
-  <div class="text-center">
-    <v-skeleton-loader type="actions" height="56" id="actionLoader" />
-  </div>
+  <v-card class="h-100 py-3">
+    <v-row>
+      <v-col cols="12" md="9">
+        <v-skeleton-loader type="text" height="56" />
+      </v-col>
+      <v-col cols="12" md="3">
+        <v-skeleton-loader type="text" height="56" />
+      </v-col>
+    </v-row>
+    <v-skeleton-loader type="alert" />
+    <v-skeleton-loader id="test" type="text" height="56" />
+    <v-skeleton-loader type="text" height="56" />
+    <v-skeleton-loader type="text" height="56" />
+    <div class="text-center">
+      <v-skeleton-loader type="actions" height="56" id="actionLoader" />
+    </div>
+  </v-card>
 </template>
 
 <style>
