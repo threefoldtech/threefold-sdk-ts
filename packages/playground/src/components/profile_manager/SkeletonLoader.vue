b3c9975744091ceda55c7b460f60842aedbd0422
Omar Kassem <omarkassem099@gmail.com>
2025-01-29 11:26:37 +0200

    Chore(ProfileManager:skeletonLoader): add ids to elements, and enhance style

--- packages/playground/src/components/profile_manager/SkeletonLoader.vue
+++ packages/playground/src/components/profile_manager/SkeletonLoader.vue
@@ -1,29 +1,41 @@
 <template>
-  <v-card class="h-100 py-3">
+  <v-card id="skeleton-card" class="h-100 py-3">
+    <v-skeleton-loader id="skeleton-card__alert" type="text" />
     <v-row>
-      <v-col cols="12" md="9">
-        <v-skeleton-loader type="text" height="56" />
+      <v-col id="skeleton-card__col-main" cols="12" md="9">
+        <v-skeleton-loader id="skeleton-card__col-main-text" type="text" height="56" />
       </v-col>
-      <v-col cols="12" md="3">
-        <v-skeleton-loader type="text" height="56" />
+      <v-col id="skeleton-card__col-secondary" cols="12" md="3">
+        <v-skeleton-loader id="skeleton-card__col-secondary-text" type="text" height="56" />
       </v-col>
     </v-row>
-    <v-skeleton-loader type="alert" />
-    <v-skeleton-loader id="test" type="text" height="56" />
-    <v-skeleton-loader type="text" height="56" />
-    <v-skeleton-loader type="text" height="56" />
-    <div class="text-center">
-      <v-skeleton-loader type="actions" height="56" id="actionLoader" />
+    <v-skeleton-loader id="skeleton-card__alert-secondary" type="alert" />
+    <v-skeleton-loader id="skeleton-card__text-test" type="text" height="56" />
+    <v-skeleton-loader id="skeleton-card__text-1" type="text" height="56" />
+    <v-skeleton-loader id="skeleton-card__text-2" type="text" height="56" />
+    <div id="skeleton-card__actions">
+      <v-skeleton-loader
+        id="skeleton-card__actions-loader"
+        class="skeleton-card__actions-loader"
+        type="actions"
+        height="56"
+      />
     </div>
   </v-card>
 </template>
 
 <style>
-#actionLoader .v-skeleton-loader__actions {
+#skeleton-card__actions .v-skeleton-loader__actions {
   justify-content: center;
 }
 .v-skeleton-loader__text {
   height: 2rem;
 }
+#skeleton-card__actions .v-skeleton-loader__actions .v-skeleton-loader__button {
+  max-width: 6rem;
+}
+
+#skeleton-card__alert .v-skeleton-loader__text {
+  height: 3rem;
+}
 </style>
-<script lang="ts" setup></script>
